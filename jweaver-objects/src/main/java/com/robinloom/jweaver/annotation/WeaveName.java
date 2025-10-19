db13dac6aec2b46d8338709c704b02a4a752d471
robinloom <mail@robinloom.com>
2025-10-19 20:12:23 +0200

    Added JavaDoc for new annotations

--- jweaver-objects/src/main/java/com/robinloom/jweaver/annotation/WeaveName.java
+++ jweaver-objects/src/main/java/com/robinloom/jweaver/annotation/WeaveName.java
@@ -5,6 +5,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Overrides the displayed name of a field, method, or record component
+ * in JWeaver's textual output.
+ * <p>
+ * This is useful when the internal field name differs from the desired
+ * representation, or when you want to abbreviate or localize property names.
+ * <p>
+ * When present, {@code @WeaveName} takes precedence over the default
+ * field or accessor name determined by reflection.
+ *
+ * <pre>{@code
+ * public class Address {
+ *     @WeaveName("zip")
+ *     private String postalCode;
+ * }
+ * }</pre>
+ *
+ * @since 2.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface WeaveName {
