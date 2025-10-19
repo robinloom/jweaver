7abb69e5ba7c6b21383204c98fe2b49d0e2f090b
robinloom <mail@robinloom.com>
2025-10-19 20:06:18 +0200

    Added annotations @WeaveRedact, @WeaveIgnore, @WeaveName

---
+++ jweaver-objects/src/test/java/com/robinloom/jweaver/annotation/WeaveNameTest.java
@@ -0,0 +1,40 @@
+package com.robinloom.jweaver.annotation;
+
+import com.robinloom.jweaver.JWeaver;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class WeaveNameTest {
+
+    record Person(String name, @WeaveName("Mom") String nameOfTheirMother, @WeaveName("Dad") String nameOfTheirFather) {
+    }
+
+    @Test
+    void testDynamicWeaver() {
+        WeaveNameTest.Person person = new WeaveNameTest.Person("John", "Gina", "Martin");
+        String expected = "Person[name=John, Mom=Gina, Dad=Martin]";
+        Assertions.assertEquals(expected, JWeaver.getDynamic().weave(person));
+    }
+
+    @Test
+    void testTreeWeaver() {
+        WeaveNameTest.Person person = new WeaveNameTest.Person("John", "Gina", "Martin");
+        String expected = """
+                          Person
+                          |-- name=John
+                          |-- Mom=Gina
+                          `-- Dad=Martin""";
+        Assertions.assertEquals(expected, JWeaver.getTree().weave(person));
+    }
+
+    @Test
+    void testBulletWeaver() {
+        WeaveNameTest.Person person = new WeaveNameTest.Person("John", "Gina", "Martin");
+        String expected = """
+                          Person
+                           - name=John
+                           - Mom=Gina
+                           - Dad=Martin""";
+        Assertions.assertEquals(expected, JWeaver.getBullet().weave(person));
+    }
+}
